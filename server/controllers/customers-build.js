a8ff5142d944b8b68481ad2e09eb5874d9937cec
SerjRide <quicksquicks6@gmail.com>
2019-04-11 21:06:02 +0300

    delete extra

--- server/controllers/customers-build.js
+++
@@ -1,9 +0,0 @@
-const customers = require('../../etc/template-customers.json');
-
-const customersBuild = (obj) => {
-  console.log('Customers Build is running...')
-  console.log(obj)
-  return customers;
-}
-
-export default customersBuild;
